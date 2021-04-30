f93fc3194bff0202161688f27022352c7daaed84
ezikechris <christophermbe1009@gmail.com>
2021-04-30 16:07:21 +0100

    added about page

--- src/components/whoweare/index.jsx
+++ src/components/whoweare/index.jsx
@@ -66,7 +66,7 @@ const whoweare = () => {
         </div>
       </div>
       <div className="lc">
-        <h1 className="on">Operational Facilities</h1>
+        <h1 className="on"> Co-orperate Offices</h1>
         <p>
           CCCRN has strengthened Operational and Facility structures to
           implement standardized testing protocols for HIV testing and
@@ -92,6 +92,271 @@ const whoweare = () => {
             </div>
           </div>
         </div>
+        <div className="fc">
+          <h1>Facilities & Research Capacity</h1>
+          <p>
+            CCCRN has implemented clinical and operational research activities
+            funded through CDC, NIH, Clinton Health Access Initiatives, GEANCO
+            and other private streams. These include Adolescent Coordinated
+            Transition study to Improve Health Outcomes among Nigerian HIV
+            Youth, (NIH 1R01HD089871-01), Tuberculosis prevalence survey among
+            HIV infected clients in South East Nigeria (CDC 5U2GGH00868-5),
+            In-service Training Impact Evaluation study (I-TIE) to assess impact
+            of in-service training on work practices in Nigeria
+            (5U2GGH000916-04), Building and Strengthening Infection Control
+            Strategies to Prevent Tuberculosis in Health facilities in South
+            East Nigeria (CDC 5U2GGH00868-03) and Intervention for Sustained
+            Testing and Retention (iSTAR) Among HIV-infected Patients (NIH
+            1R01HD087994-01), among others.
+          </p>
+          <div className="rw">
+            <div className="facilities-col">
+              <img src="images/Image13.jpg" alt="facilities" />
+              <h3>Disease Surveillance and Notification</h3>
+              <p>
+                {" "}
+                In consortium with the University of Maryland, CCCRN was awarded
+                GH15-1632 Global Health Security project to expand efforts and
+                strategies to protect and improve public health globally.
+                Following the 2015 Ebola Virus epidemic, the project
+                strengthened reporting systems for early detection and response
+                for diseases of public health importance.
+              </p>
+            </div>
+            <div className="facilities-col">
+              <img src="images/Image15.jpg" alt="facilities" />
+              <h3> Service Delivery and Treatment</h3>
+              <p>
+                From 2010 till date, CCCRN has implemented large comprehensive
+                HIV care and treatment projects (CDC/CHAN-CHARIS Project:
+                2010–2012; CDC/SEEDS project 2012-2017; CDC/APIN iCARES project:
+                2017-2022) in Plateau, Kano, Enugu, Ebonyi Imo and Benue States,
+                OVC services in six states(Imo, Ebonyi, Enugu, Cross River, Akwa
+                Ibom and Benue State).
+              </p>
+            </div>
+            <div className="facilities-col">
+              <img src="images/Image10.jpg" alt="facilities" />
+              <h3>Health Information Systems</h3>
+              <p>
+                Through our capacity building and Technological infrastructure,
+                CCCRN has established a web based platforms to provide virtual
+                capacity building opportunities for various health care work
+                forces. It provides additional learning resource other than
+                those covered in the conventional training sessions.
+              </p>
+            </div>
+          </div>
+          <div className="tm">
+            <h1>Experience To Go The Distance</h1>
+            <p>Our Backers Ratings</p>
+            <div className="rw">
+              <div className="testimonial-col">
+                <img src="images/cdc.jpg" alt="cdc logo" />
+                <div className="">
+                  <p>
+                    Through our partnership, cccrn continues to expand its core
+                    competencies to be a leading organization for health care
+                    solution in Nigeria.
+                  </p>
+                  <h3>CDC</h3>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star-half"></i>
+                </div>
+              </div>
+              <div className="testimonial-col">
+                <img src="images/usaid2.jpg" alt="usaid logo" />
+                <div className="">
+                  <p>
+                    Through our partnership, cccrn continues to expand its core
+                    competencies to be a leading organization for health care
+                    solution in Nigeria.
+                  </p>
+                  <h3>USAID</h3>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star-half"></i>
+                </div>
+              </div>
+              <div className="testimonial-col">
+                <img src="images/ncdc.jpg" alt="ncdc logo" />
+                <div className="">
+                  <p>
+                    Through our partnership, cccrn continues to expand its core
+                    competencies to be a leading organization for health care
+                    solution in Nigeria.
+                  </p>
+                  <h3>NCDC</h3>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star"></i>
+                  <i className="fa fa-star-half"></i>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div>
+        <footer id="footer" class="footer-area">
+          <div class="footer-top">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-3 col-md-6">
+                  <div class="footer-widget about_widget">
+                    <a href="index.html" class="footer-logo"></a>
+                    <p>
+                      Our guiding philosophy is to provide best possible care
+                      solutions for each client, magnifying same approach to
+                      reach many more in reaching public health goals.{" "}
+                    </p>
+                    <ul class="social-icon">
+                      <li>
+                        <a href="#">
+                          <i class="fab fa-facebook-f" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fab fa-twitter" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fab fa-linkedin-in" />
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fab fa-pinterest-p" />
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                  <div class="footer-widget nav_menus_widget">
+                    <h4 class="widget-title">Useful Links</h4>
+                    <ul>
+                      <li>
+                        <a href="index.html">
+                          <i class="fas fa-chevron-right" /> Home
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> About Us
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Service
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Blog
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Contact
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                  <div class="footer-widget nav_menus_widget">
+                    <h4 class="widget-title">Need Help?</h4>
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Faqs
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Privacy
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Policy
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Support
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Terms
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+                <div class="col-lg-3 col-md-6">
+                  <div class="footer-widget nav_menus_widget">
+                    <h4 class="widget-title">More Links</h4>
+                    <ul>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Careers
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Projects
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Missions
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> Team
+                        </a>
+                      </li>
+                      <li>
+                        <a href="#">
+                          <i class="fas fa-chevron-right" /> People
+                        </a>
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div class="copyright-area">
+            <div class="container">
+              <div class="row">
+                <div class="col-lg-12">
+                  <div class="copyright-inner">
+                    <div class="left-content-area">
+                      &copy; Copyrights 2020 Center for Clinical Care and
+                      Clinical Research | Plot 784, Jahi District, off Life Camp
+                      Gwarimpa by Pass Expressway, Abuja | +234-8086-664968 |
+                      Powered by Diggital Inc.
+                    </div>
+                    <div class="right-content-area" />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </footer>
       </div>
     </header>
   );
